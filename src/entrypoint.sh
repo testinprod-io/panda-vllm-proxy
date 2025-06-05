52106e62ac51642e7fbc1724c9e88deb551b8a86
Harry Jeon <94497407+sm-stack@users.noreply.github.com>
2025-06-05 16:18:57 -0400

    Apply pre-loading of Milvus client (#16)
    * Apply pre-loading Milvus wrapper
    
    * Apply timeout on AsyncHttpLoader
    
    * Increase timeout of gunicorn to 180 seconds

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -9,4 +9,5 @@ echo "Starting Gunicorn with $WORKERS workers on $HOST:$PORT"
 exec gunicorn app.main:app \
     -k uvicorn.workers.UvicornWorker \
     --workers $WORKERS \
+    --timeout 180 \
     --bind $HOST:$PORT
