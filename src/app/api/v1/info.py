de71b99eb5e357818a2435cc150aca943d5b34ca
Harry Jeon <94497407+sm-stack@users.noreply.github.com>
2025-06-05 16:21:32 -0400

    Fix info API to use /v1/models endpoint of vllm (#18)
    * Fix info API to use /v1/model endpoint of vllm
    
    * Update env vars and dependencies

--- src/app/api/v1/info.py
+++ src/app/api/v1/info.py
@@ -1,35 +1,27 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from transformers import AutoConfig
+import httpx
 
 from ...config import get_settings
-from ...logger import log
 
 router = APIRouter(tags=["info"])
 
 @router.get("/info")
 async def info():
     settings = get_settings()
-    # If MAX_MODEL_LENGTH is set, use it
-    if settings.MAX_MODEL_LENGTH:
-        ctx_len = settings.MAX_MODEL_LENGTH
-    else:
-        try:
-            cfg = AutoConfig.from_pretrained(settings.MODEL_NAME)
-            # For Deepseek models
-            if hasattr(cfg, 'max_position_embeddings'):
-                ctx_len = cfg.max_position_embeddings
-            else:
-                # For Qwen 2.5 models
-                if hasattr(cfg, 'talker_config'):
-                    ctx_len = cfg.talker_config.max_position_embeddings
-                else:
-                    return JSONResponse(status_code=400, content={"error": "Max model length not found"})
-        except Exception as e:
-            log.error(f"Error getting model info: {e}")
-            return JSONResponse(status_code=500, content={"error": "Error getting model info"})
 
-    return JSONResponse(content={
-        "ctx_len": ctx_len,
-        "model_name": settings.MODEL_NAME,
-    })
+    client = httpx.AsyncClient(timeout=httpx.Timeout(10))
+    try:
+        # Call vllm to get the model info
+        model_info = await client.get(f"{settings.VLLM_MODEL_URL}")
+        if model_info.status_code != 200:
+            return JSONResponse(status_code=500, content={"error": "Failed to get model info"})
+        model_info = model_info.json()
+        data = model_info["data"][0]
+
+        return JSONResponse(content={
+            "ctx_len": data["max_model_len"],
+            "model_name": data["id"],
+        })
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": "Failed to get model info"})
